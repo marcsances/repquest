d8db048c7b3e3c8e410128fe1b1afcfdb1d5e263
Marc Sances <marcsances@gmail.com>
2023-07-15 22:42:51 +0200

    move context up

--- src/context/workoutContext.tsx
+++ src/context/workoutContext.tsx
@@ -1,55 +1,152 @@
-import React, {ReactElement, ReactNode, useCallback, useContext, useState} from "react";
-import {LocalWorkoutExercise, Workout, WorkoutExercise} from "../models/workout";
+import React, {ReactElement, ReactNode, useCallback, useContext, useEffect, useMemo, useState} from "react";
+import {ExerciseSet, LocalWorkoutExercise, Workout, WorkoutExercise} from "../models/workout";
 import {Exercise} from "../models/exercise";
 import {DBContext} from "./dbContext";
 
 interface IWorkoutContext {
     timeStarted?: Date;
     followingWorkout?: Workout;
-    workoutExercises: LocalWorkoutExercise[];
+    workoutExercises?: LocalWorkoutExercise[];
+    currentWorkout?: Workout;
+    currentSet?: ExerciseSet;
+    currentSetNumber: number;
+    currentWorkoutExercise?: WorkoutExercise;
+    currentWorkoutExerciseNumber: number;
+    setCurrentWorkout?: (currentWorkout?: Workout) => void;
+    setCurrentSet?: (currentSet?: ExerciseSet) => void;
+    setCurrentSetNumber?: (currentSetNumber: number) => void;
+    setCurrentWorkoutExercise?: (currentWorkoutExercise?: WorkoutExercise) => void;
+    setCurrentWorkoutExerciseNumber?: (currentWorkoutExerciseNumber: number) => void;
     focusedExercise?: Exercise;
     startWorkout?: (followingWorkout?: Workout) => Promise<void>;
     saveExercise?: (workoutExercise: LocalWorkoutExercise) => void;
-    focusExercise?: (exercise: Exercise) => void;
+    setFocusedExercise?: (exercise: Exercise) => void;
 }
 
 export const WorkoutContext = React.createContext({
     timeStarted: undefined,
     followingWorkout: undefined,
     workoutExercises: [],
-    focusedExercise: undefined
+    focusedExercise: undefined,
+    currentWorkout: undefined,
+    currentSet: undefined,
+    currentSetNumber: 1,
+    currentWorkoutExercise: undefined,
+    currentWorkoutExerciseNumber: 0
 } as IWorkoutContext);
 
-export const WorkoutContextProvider = (props: {children: ReactElement}) => {
-    const { children } = props;
-    const [workoutContext, setWorkoutContext] = useState<IWorkoutContext>({workoutExercises: []});
+export const WorkoutContextProvider = (props: { children: ReactElement }) => {
+    const {children} = props;
+    const [timeStarted, setTimeStarted] = useState<Date | undefined>(undefined);
+    const [workoutExercises, setWorkoutExercises] = useState<LocalWorkoutExercise[] | undefined>(undefined);
+    const [focusedExercise, setFocusedExercise] = useState<Exercise | undefined>(undefined);
+    const [currentWorkout, setCurrentWorkout] = useState<Workout | undefined>(undefined);
+    const [currentSet, setCurrentSet] = useState<ExerciseSet | undefined>(undefined);
+    const [currentSetNumber, setCurrentSetNumber] = useState(1);
+    const [currentWorkoutExercise, setCurrentWorkoutExercise] = useState<WorkoutExercise | undefined>(undefined);
+    const [currentWorkoutExerciseNumber, setCurrentWorkoutExerciseNumber] = useState(0);
+    const [followingWorkout, setFollowingWorkout] = useState<Workout | undefined>(undefined);
     const {db} = useContext(DBContext);
+
     const startWorkout = useCallback(async (followingWorkout?: Workout) => {
-        let focusedExercise: Exercise | undefined;
-        if (followingWorkout && followingWorkout.workoutExerciseIds.length > 0) {
-            const workoutExercise = await db?.workoutExercise.get(followingWorkout.workoutExerciseIds[0]);
-            focusedExercise = workoutExercise ? await db?.exercise.get(workoutExercise.exerciseId) : undefined;
+        setTimeStarted(new Date());
+        setWorkoutExercises([]);
+        setFocusedExercise(undefined);
+        setCurrentSetNumber(1);
+        setCurrentWorkoutExerciseNumber(0);
+        setFollowingWorkout(followingWorkout);
+        setFocusedExercise(undefined);
+        setCurrentSet(undefined);
+    }, [db, setTimeStarted, setWorkoutExercises, setCurrentSetNumber, setCurrentWorkoutExerciseNumber, setFollowingWorkout, setFocusedExercise]);
+
+    useEffect(() => {
+        if (!setCurrentWorkout) return;
+        if (!followingWorkout) {
+            setCurrentWorkout(undefined);
+            return;
         }
-        setWorkoutContext({
-            timeStarted: new Date(),
-            followingWorkout,
-            workoutExercises: [],
-            focusedExercise
-        })
-    }, [db, setWorkoutContext]);
-    const saveExercise = useCallback(async (workoutExercise: LocalWorkoutExercise) => {
-        setWorkoutContext((prevContext) => ({
-            ...prevContext,
-            workoutExercises: prevContext.workoutExercises.concat([workoutExercise])
-        }))
-    }, [db, setWorkoutContext]);
-    const focusExercise = useCallback(async (exercise: Exercise) => {
-        setWorkoutContext((prevContext) => ({
-            ...prevContext,
-            focusedExercise: exercise
-        }))
-    }, [setWorkoutContext]);
-    return <WorkoutContext.Provider value={{...workoutContext, startWorkout, saveExercise, focusExercise}}>
+        db?.workout.get(followingWorkout.id).then((workout) => {
+            if (workout) {
+                setCurrentWorkout(workout);
+            }
+        });
+    }, [followingWorkout, setCurrentWorkout, db]);
+
+    useEffect(() => {
+        if (!setCurrentWorkoutExercise) return;
+        if (!currentWorkout) {
+            setCurrentWorkoutExercise(undefined);
+            return;
+        }
+        db?.workoutExercise.get(currentWorkout.workoutExerciseIds[currentWorkoutExerciseNumber]).then((workoutExercise) => {
+            if (workoutExercise) {
+                setCurrentWorkoutExercise(workoutExercise);
+            }
+        });
+    }, [currentWorkout, currentWorkoutExerciseNumber, setCurrentWorkoutExercise, db]);
+
+    useEffect(() => {
+        if (!setCurrentSet) return;
+        if (!currentWorkoutExercise) {
+            setCurrentSet(undefined);
+            return;
+        }
+        db?.exerciseSet.get(currentWorkoutExercise.setIds[currentSetNumber - 1]).then((set) => {
+            if (set) {
+                setCurrentSet(set);
+            }
+        });
+    }, [currentWorkoutExercise, currentSetNumber, setCurrentSet, db]);
+
+    useEffect(() => {
+        if (!currentWorkoutExercise) {
+            return;
+        }
+        db?.exercise.get(currentWorkoutExercise.exerciseId).then((exercise) => {
+            if (exercise && setFocusedExercise) {
+                setFocusedExercise(exercise);
+            }
+        });
+    }, [currentWorkoutExercise, setFocusedExercise, db]);
+
+    const saveExercise = useCallback((workoutExercise: LocalWorkoutExercise) => {
+        setWorkoutExercises((prevExercises) => prevExercises?.concat([workoutExercise]));
+    }, [db, setWorkoutExercises]);
+
+    const context = useMemo(() => ({
+        timeStarted,
+        followingWorkout,
+        workoutExercises,
+        focusedExercise,
+        currentWorkout,
+        currentSet,
+        currentSetNumber,
+        currentWorkoutExercise,
+        currentWorkoutExerciseNumber,
+        setCurrentWorkout,
+        setCurrentSet,
+        setCurrentSetNumber,
+        setCurrentWorkoutExercise,
+        setFocusedExercise,
+        setCurrentWorkoutExerciseNumber,
+        startWorkout, saveExercise
+    }), [timeStarted,
+        followingWorkout,
+        workoutExercises,
+        focusedExercise,
+        currentWorkout,
+        currentSet,
+        currentSetNumber,
+        currentWorkoutExercise,
+        currentWorkoutExerciseNumber,
+        setCurrentWorkout,
+        setCurrentSet,
+        setCurrentSetNumber,
+        setCurrentWorkoutExercise,
+        setFocusedExercise,
+        setCurrentWorkoutExerciseNumber,
+        startWorkout, saveExercise])
+    return <WorkoutContext.Provider value={context}>
         {children}
     </WorkoutContext.Provider>
 }
--- src/pages/workout/workout.tsx
+++ src/pages/workout/workout.tsx
@@ -19,73 +19,47 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import DoneIcon from '@mui/icons-material/Done';
-import AddIcon from '@mui/icons-material/Add';
 import Parameter from "../../components/parameter";
 import SetParameter from "../../components/setParameter";
 import {useTranslation} from "react-i18next";
 import {WorkoutContext} from "../../context/workoutContext";
 import {DBContext} from "../../context/dbContext";
-import {ExerciseSet, Workout, WorkoutExercise} from "../../models/workout";
 import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import {useNavigate} from "react-router-dom";
 
 export const WorkoutPage = () => {
-    const {timeStarted, followingWorkout, workoutExercises, focusedExercise, focusExercise} = useContext(WorkoutContext);
+    const {
+        timeStarted,
+        followingWorkout,
+        workoutExercises,
+        focusedExercise,
+        currentWorkout,
+        currentSet,
+        currentSetNumber,
+        currentWorkoutExercise,
+        currentWorkoutExerciseNumber,
+        setCurrentWorkout,
+        setCurrentSet,
+        setCurrentSetNumber,
+        setCurrentWorkoutExercise,
+        setFocusedExercise,
+        setCurrentWorkoutExerciseNumber
+    } = useContext(WorkoutContext);
     const {db} = useContext(DBContext);
     const [viewHistory, setViewHistory] = useState(false);
     const {t} = useTranslation();
-    const [currentWorkout, setCurrentWorkout] = useState<Workout | undefined>(undefined);
-    const [currentSet, setCurrentSet] = useState<ExerciseSet | undefined>(undefined);
-    const [currentSetNumber, setCurrentSetNumber] = useState(1);
-    const [currentWorkoutExercise, setCurrentWorkoutExercise] = useState<WorkoutExercise | undefined>(undefined);
-    const [currentWorkoutExerciseNumber, setCurrentWorkoutExerciseNumber] = useState(0);
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!followingWorkout) { setCurrentWorkout(undefined); return; }
-        db?.workout.get(followingWorkout.id).then((workout) => {
-            if (workout) {
-                setCurrentWorkout(workout);
-            }
-        });
-    }, [followingWorkout, setCurrentWorkout, db]);
-
-    useEffect(() => {
-        if (!currentWorkout) { setCurrentWorkoutExercise(undefined); return; }
-        db?.workoutExercise.get(currentWorkout.workoutExerciseIds[currentWorkoutExerciseNumber]).then((workoutExercise) => {
-            if (workoutExercise) {
-                setCurrentWorkoutExercise(workoutExercise);
-            }
-        });
-    }, [currentWorkout, currentWorkoutExerciseNumber, setCurrentWorkoutExercise, db]);
-
-    useEffect(() => {
-        if (!currentWorkoutExercise) { setCurrentSet(undefined); return; }
-        db?.exerciseSet.get(currentWorkoutExercise.setIds[currentSetNumber - 1]).then((set) => {
-            if (set) {
-                setCurrentSet(set);
-            }
-        });
-    }, [currentWorkoutExercise, currentSetNumber, setCurrentSet, db]);
-
-    useEffect(() => {
-        if (!currentWorkoutExercise) { return; }
-        db?.exercise.get(currentWorkoutExercise.exerciseId).then((exercise) => {
-            if (exercise && focusExercise) {
-                focusExercise(exercise);
-            }
-        });
-    }, [currentWorkoutExercise, focusExercise, db]);
-
-    return <Layout title={followingWorkout?.name || t("freeTraining")} hideNav toolItems={focusedExercise?.yt_video ? <IconButton
-        color="inherit"
-        aria-label="menu"
-        sx={{mr: 2}}
-        onClick={() => navigate("/youtube")}
-    >
-        <YouTubeIcon/>
-    </IconButton> : <></>}>
+    return <Layout title={followingWorkout?.name || t("freeTraining")} hideNav
+                   toolItems={focusedExercise?.yt_video ? <IconButton
+                       color="inherit"
+                       aria-label="menu"
+                       sx={{mr: 2}}
+                       onClick={() => navigate("/youtube")}
+                   >
+                       <YouTubeIcon/>
+                   </IconButton> : <></>}>
         <Box sx={{height: "100%", display: "flex", flexDirection: "column"}}>
             <Paper variant="outlined">
                 <CardActionArea onClick={() => setViewHistory(!viewHistory)}>
@@ -116,7 +90,7 @@ export const WorkoutPage = () => {
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {focusedExercise && workoutExercises.filter((it) => it.id === focusedExercise.id).flatMap((exercise) => exercise.sets.map((set, idx) =>
+                                    {focusedExercise && workoutExercises && workoutExercises.filter((it) => it.id === focusedExercise.id).flatMap((exercise) => exercise.sets.map((set, idx) =>
                                         <TableRow
                                             key={set.id}
                                             sx={{'&:last-child td, &:last-child th': {border: 0}}}
@@ -136,20 +110,28 @@ export const WorkoutPage = () => {
                         </TableContainer>}
                 </CardActionArea>
             </Paper>
-            <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-                {currentWorkout?.workoutExerciseIds && currentWorkoutExerciseNumber > 0 && <IconButton aria-label="previous" onClick={() => setCurrentWorkoutExerciseNumber((prevNumber) => prevNumber - 1)}>
-                    <ArrowLeftIcon />
-                </IconButton>}
-                {currentWorkout?.workoutExerciseIds && currentWorkoutExerciseNumber < currentWorkout?.workoutExerciseIds.length - 1 && <IconButton aria-label="next"onClick={() => setCurrentWorkoutExerciseNumber((prevNumber) => prevNumber + 1)}>
-                    <ArrowRightIcon />
-                </IconButton>}
+            <Box sx={{display: 'flex', alignItems: 'center', pl: 1, pb: 1}}>
+                {currentWorkout?.workoutExerciseIds && setCurrentWorkoutExerciseNumber && currentWorkoutExerciseNumber > 0 &&
+                    <IconButton aria-label="previous"
+                                onClick={() => setCurrentWorkoutExerciseNumber(currentWorkoutExerciseNumber - 1)}>
+                        <ArrowLeftIcon/>
+                    </IconButton>}
+                {currentWorkout?.workoutExerciseIds && setCurrentWorkoutExerciseNumber && currentWorkoutExerciseNumber < currentWorkout?.workoutExerciseIds.length - 1 &&
+                    <IconButton aria-label="next"
+                                onClick={() => setCurrentWorkoutExerciseNumber(currentWorkoutExerciseNumber + 1)}>
+                        <ArrowRightIcon/>
+                    </IconButton>}
             </Box>
-            <SetParameter name={t("set")} value={currentSetNumber} min={1} max={currentWorkoutExercise?.setIds.length} incrementBy={1} onChange={(setNumber) => setCurrentSetNumber(setNumber)}/>
-            {currentSet?.weight && <Parameter name={t("weight")} unit="kg" value={currentSet?.weight} min={0} incrementBy={2.5} allowDecimals/>}
+            <SetParameter name={t("set")} value={currentSetNumber} min={1} max={currentWorkoutExercise?.setIds.length}
+                          incrementBy={1} onChange={(setNumber) => { if (setCurrentSetNumber) setCurrentSetNumber(setNumber)}}/>
+            {currentSet?.weight &&
+                <Parameter name={t("weight")} unit="kg" value={currentSet?.weight} min={0} incrementBy={2.5}
+                           allowDecimals/>}
             {currentSet?.reps && <Parameter name={t("reps")} value={currentSet?.reps} min={1} incrementBy={1}/>}
             {currentSet?.rpe && <Parameter name={t("rpe")} value={currentSet?.rpe} min={0} max={10} incrementBy={1}/>}
             {currentSet?.rir && <Parameter name={t("rir")} value={currentSet?.rir} min={0} incrementBy={1}/>}
-            {currentSet?.rest && <Parameter name={t("rest")} unit="s" value={currentSet?.rest} min={0} incrementBy={10}/>}
+            {currentSet?.rest &&
+                <Parameter name={t("rest")} unit="s" value={currentSet?.rest} min={0} incrementBy={10}/>}
             <Box sx={{flexGrow: 1}}/>
             <Stack direction="row" spacing={{xs: 1, sm: 2, md: 4}} sx={{alignSelf: "center", marginBottom: "24px"}}>
                 <Fab color="primary" aria-label="add">
